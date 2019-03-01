a2b7a888d57570f4334ed98deabcb632177a401f
Eduardo <eduu.porto@gmail.com>
2019-03-01 17:56:31 -0300

    Toto

--- src/app/database/database.ts
+++ src/app/database/database.ts
@@ -1,4 +1,18 @@
+import {getManager} from "typeorm";
+import { User } from '../entity/User';
+import { findNode } from '@angular/compiler';
 
 export class GenericDao {
+	static  entityManager = getManager(); // you can also get it via getConnection().manager
+}
 
+export class UserDao extends GenericDao {
+
+	async findNode(id : number){
+		return await UserDao.entityManager.findOne(User, 1);
+	}
+
+	async save(user : User){
+		await UserDao.entityManager.save(user);
+	}
 }
