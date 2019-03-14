bd55c23bed264a6c895ec2c33add4a0a24f3f2b9
Eduardo <eduu.porto@gmail.com>
2019-03-14 12:40:55 -0300

    WebService

---
+++ TotoWebService/src/main/java/br/com/duti/petlife/services/UserService.java
@@ -0,0 +1,20 @@
+package br.com.duti.petlife.services;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import br.com.duti.petlife.models.User;
+
+@Path("/toto/user")
+public class UserService {
+	
+	@GET
+	@Produces( { MediaType.APPLICATION_JSON })
+	 public User get(@PathParam("login") String login, @PathParam("id") String password) {
+		final User user = new User();
+		return user;
+	 }
+}
