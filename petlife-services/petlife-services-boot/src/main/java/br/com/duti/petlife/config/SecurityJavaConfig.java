14c42a94d823d61b451a40a80e498b3b36adf477
Eduardo <eduu.porto@gmail.com>
2019-03-21 17:39:55 -0300

    WebService

--- petlife-services/petlife-services-boot/src/main/java/br/com/duti/petlife/config/SecurityJavaConfig.java
+++ petlife-services/petlife-services-boot/src/main/java/br/com/duti/petlife/config/SecurityJavaConfig.java
@@ -14,73 +14,73 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
-//@Configuration
-//@EnableWebSecurity
-//@EnableGlobalMethodSecurity(prePostEnabled = true)
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 //@Order(SecurityProperties.ACCESS_OVERRIDE_ORDER)
-public class SecurityJavaConfig /*extends WebSecurityConfigurerAdapter*/ {
+public class SecurityJavaConfig extends WebSecurityConfigurerAdapter {
 	
-//	private static final String ADMIN = "ADMIN";
-//	private static final String USER = "USER";
-//	private static final String admin = "admin";
-//	private static final String user = "user";
-//	
-//	@Autowired
-//	private ServletContext servletContext;
-//	
-//	@Autowired
-//	private RestAuthenticationEntryPoint restAuthenticationEntryPoint;
-//	
-//	@Override
-//	protected void configure(final AuthenticationManagerBuilder auth) throws Exception {
-//		if(auth == null)
-//			return;
-//		
-//	    auth.inMemoryAuthentication()
-//	        .withUser(admin).password(encoder().encode("adminPass")).roles(ADMIN)
-//	        .and()
-//	        .withUser(user).password(encoder().encode("userPass")).roles(USER);
-//	}
-//	
-//	@Override
-//	protected void configure(final HttpSecurity http) throws Exception { 
-//		if(http == null)
-//			return;
-//		
-//		http
-//	    .csrf()
-//	        .disable()
-//	   .exceptionHandling()
-//	       .authenticationEntryPoint(restAuthenticationEntryPoint)
-//	       .and()
-//	   .sessionManagement()
-//	       .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-//	       .and()
-//	   .authorizeRequests()
-//	       .antMatchers("/",
-//	           "/favicon.ico",
-//	           "/**/*.png",
-//	           "/**/*.gif",
-//	           "/**/*.svg",
-//	           "/**/*.jpg",
-//	           "/**/*.html",
-//	           "/**/*.css",
-//	           "/**/*.js")
-//	           .permitAll()
-//	        .antMatchers("/auth/**")
-//	           .permitAll()
-//	        .antMatchers("/auth/login")
-//	           .permitAll()
-//	       .antMatchers("/permissions")
-//	           .permitAll()
-//	       .anyRequest()
-//	           .authenticated();
-//		
-//		//http.addFilterBefore(jwtAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
-//	}
-//	 
-//	@Bean
-//	public PasswordEncoder encoder() {
-//	    return new BCryptPasswordEncoder();
-//	}
+	private static final String ADMIN = "ADMIN";
+	private static final String USER = "USER";
+	private static final String admin = "admin";
+	private static final String user = "user";
+	
+	@Autowired
+	private ServletContext servletContext;
+	
+	@Autowired
+	private RestAuthenticationEntryPoint restAuthenticationEntryPoint;
+	/*
+	@Override
+	protected void configure(final AuthenticationManagerBuilder auth) throws Exception {
+		if(auth == null)
+			return;
+		
+	    auth.inMemoryAuthentication()
+	        .withUser(admin).password(encoder().encode("adminPass")).roles(ADMIN)
+	        .and()
+	        .withUser(user).password(encoder().encode("userPass")).roles(USER);
+	}*/
+	
+	@Override
+	protected void configure(final HttpSecurity http) throws Exception { 
+		if(http == null)
+			return;
+		
+		http
+	    .csrf()
+	        .disable()
+	   .exceptionHandling()
+	       .authenticationEntryPoint(restAuthenticationEntryPoint)
+	       .and()
+	   .sessionManagement()
+	       .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+	       .and()
+	   .authorizeRequests()
+	       .antMatchers("/",
+	           "/favicon.ico",
+	           "/**/*.png",
+	           "/**/*.gif",
+	           "/**/*.svg",
+	           "/**/*.jpg",
+	           "/**/*.html",
+	           "/**/*.css",
+	           "/**/*.js")
+	           .permitAll()
+	        .antMatchers("/user/**")
+	           .permitAll()
+	        .antMatchers("/user/autheticate")
+	           .permitAll()
+	       .antMatchers("/permissions")
+	           .permitAll()
+	       .anyRequest()
+	           .authenticated();
+		
+		//http.addFilterBefore(jwtAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
+	}
+	 
+	@Bean
+	public PasswordEncoder encoder() {
+	    return new BCryptPasswordEncoder();
+	}
 }
